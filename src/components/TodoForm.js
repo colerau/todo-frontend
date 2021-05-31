17dc5dc2db39c18209a3e818bb8d37058be0930f
Cole Rau <colevrau@gmail.com>
2021-05-31 16:08:00 -0700

    installed proptypes and datepicker

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react' 
+import DatePicker from 'react-datepicker'
 
 const TodoForm = props => {
 
+  const handleChange = () => {
 
+  }
+
+  const handleSubmit = () => {
+
+  }
 
   return (
     <>
       <h1>New Item</h1>
+      <form onSubmit={handleSubmit}>
+      </form>
     </>
   )
 }
