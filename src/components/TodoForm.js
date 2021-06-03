16cae9590fff921d6bc4230a7a7191e2e0e618ad
Cole Rau <colevrau@gmail.com>
2021-06-03 16:35:06 -0700

    added failure message when user submits blank description

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,7 +4,9 @@ import DatePicker from 'react-datepicker'
 const TodoForm = props => {
 
   const [description, setDescription] = useState('')
-  const [alert, setAlert] = useState(false)
+  const [successAlert, setSuccessAlert] = useState(false)
+  const [failureAlert, setFailureAlert] = useState(false)
+  const [failureMessage, setFailureMessage] = useState('')
 
   const descriptionChange = (event) => {
     setDescription(event.target.value)
@@ -26,7 +28,12 @@ const TodoForm = props => {
     .then(data => {
       console.log(data);
       setDescription('')
-      setAlert(true)
+      if (data.status === "error") {
+        setFailureAlert(true)
+        setFailureMessage(data.messages[0])
+      } else if (data.status === "success") {
+        setSuccessAlert(true)
+      }
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -38,7 +45,13 @@ const TodoForm = props => {
     <>
       <h1>New Item</h1>
       <form onSubmit={handleSubmit}>
-        {alert && <h2 className="success">Submit Successful</h2>}
+        {successAlert && <h2 className="success">Submit Successful</h2>}
+        {failureAlert && 
+          <div>
+            <h2 className="failure">Failed to Submit</h2>
+            <h3 className="failure">{failureMessage}</h3>
+          </div>
+        }
         <label>
           Description
           <input type="text" name="description" value={description} onChange={descriptionChange} />
